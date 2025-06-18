eef1a01c3b7a06b9c7594ea1709b59772e8097c7
truongtrong99 <truongtrong400@gmail.com>
2025-06-18 16:28:43 +0700

    #20 - Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Hello World
+          Hello World! This is a simple Vite + React application.
         </p>
       </div>
       <p className="read-the-docs">
